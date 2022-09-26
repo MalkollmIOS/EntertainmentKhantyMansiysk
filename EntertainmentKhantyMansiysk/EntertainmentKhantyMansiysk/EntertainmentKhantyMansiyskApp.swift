87f799a4946298bcad21e4dcb9d7044af636df3f
Marsel <maergon@yandex.ru>
2022-09-27 00:00:33 +0500

    New project

---
+++ EntertainmentKhantyMansiysk/EntertainmentKhantyMansiysk/EntertainmentKhantyMansiyskApp.swift
@@ -0,0 +1,17 @@
+//
+//  EntertainmentKhantyMansiyskApp.swift
+//  EntertainmentKhantyMansiysk
+//
+//  Created by Марсель Фаткуллин on 26.09.2022.
+//
+
+import SwiftUI
+
+@main
+struct EntertainmentKhantyMansiyskApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
